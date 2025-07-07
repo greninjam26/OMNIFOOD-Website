eb81383f6eb9d26bd9f2b07f86ab8dcdfcff73eb
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-06 22:08:59 -0400

    addition fixes

--- Javascript/script.js
+++ Javascript/script.js
@@ -31,9 +31,16 @@ allLinks.forEach(function (link) {
                 behavior: "smooth",
             });
         }
+
+        // Scroll to other links
         if (href !== "#" && href.startsWith("#")) {
             const sectionEl = document.querySelector(href);
             sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+
+        // Close mobile navigation
+        if (link.classList.contains("main-nav-link")) {
+            headerEl.classList.toggle("nav-open");
         }
     });
 });
