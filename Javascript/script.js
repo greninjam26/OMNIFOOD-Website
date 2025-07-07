630f42790e07ed683c3ed3087fa8ec4e204f3b5d
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-06 22:03:54 -0400

    finish the main nav

--- Javascript/script.js
+++ Javascript/script.js
@@ -9,6 +9,41 @@
 //     h1.style.backgroundColor = "red";
 // });
 
+////////////////////////////////////////////////////////////////////////////
+// set current year
 const yearEL = document.querySelector(".year");
 const curYear = new Date().getFullYear();
 yearEL.textContent = curYear;
+
+////////////////////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+// might not need safari works with CSS now
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        // Scroll back to the top
+        if (href === "#") {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
+
+////////////////////////////////////////////////////////////////////////////
+// make mobile nav work
+
+const btnNavEL = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEL.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+});
