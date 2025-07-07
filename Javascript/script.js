e800a79e8897ac899d212a6b596b0fa330611860
Gavin Liu <Gavin.liuzm@gmail.com>
2025-07-07 08:34:23 -0400

    add sticky main nav

--- Javascript/script.js
+++ Javascript/script.js
@@ -46,6 +46,30 @@ allLinks.forEach(function (link) {
 });
 
 ////////////////////////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        if (!ent.isIntersecting) {
+            document.body.classList.add("sticky");
+        }
+        if (ent.isIntersecting) {
+            document.body.classList.remove("sticky");
+        }
+    },
+    {
+        // In the viewport
+        root: null,
+        threshold: 0,
+        rootMargin: "-80px",
+    }
+);
+obs.observe(sectionHeroEl);
+
+////////////////////////////////////////////////////////////////////////////
 // make mobile nav work
 
 const btnNavEL = document.querySelector(".btn-mobile-nav");
